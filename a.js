e3e3957837d5f2850aa666ce320392f0d765dc82
vikash <vicky.desti.99@gmail.com>
2025-01-18 16:04:40 +0530

    change1

---
+++ a.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 1;
+function greet(firstName) {
+    console.log(`Hello, ${firstName}`);
+}
+greet("John");
---
+++ a.ts
@@ -0,0 +1,7 @@
+let a : number = 1;
+
+function greet (firstName: string) {
+    console.log(`Hello, ${firstName}`);
+}
+
+greet("John");
