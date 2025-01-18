bc66fdaf0dd22587d107e200f975f6e5ae2220d5
vikash <vicky.desti.99@gmail.com>
2025-01-18 17:06:55 +0530

    callback

--- a.ts
+++ a.ts
@@ -7,8 +7,22 @@ function greet (firstName: string) {
 greet("John");
 
 // SUM Function
+// Type Inferrence
+
 function sum(a: number, b: number): number {
     return a + b;
 }
 const result = sum(1, 2);
 console.log(result);
+
+
+//callback funtion -> passing a function as an argument to another function
+// Thing to learn is how to give type to a function
+
+function delay (oneSec : () => void) {
+    setTimeout(oneSec, 1000);
+}
+
+function oneSec() {
+    console.log("1 second has passed");
+}
